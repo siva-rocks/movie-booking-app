2bb6e1e54b58cf0be87c2a883a2f6457ddf1c909
siva-rocks <130919675+siva-rocks@users.noreply.github.com>
2023-04-16 21:44:16 +0530

    Update moviesStore.js

--- src/stores/moviesStore.js
+++ src/stores/moviesStore.js
@@ -3,59 +3,59 @@ import { readable } from 'svelte/store';
 const movies = readable([
 {
 	id: 1,
-	name: 'Incredibles 2',
+	name: 'OG',
 	price: 299,
-	portrait: '/assets/images/Incredibles_2/portrait.jpg',
-	landscape: '/assets/images/Incredibles_2/landscape.jpg'
+	portrait: '/assets/images/OG/portrait.jpg',
+	landscape: '/assets/images/OG/landscape.jpg'
 },
 {
 	id: 2,
-	name: 'Alita Battle Angel',
+	name: 'Pushpa 2',
 	price: 249,
-	portrait: '/assets/images/Alita Battle Angel/portrait.png',
-	landscape: '/assets/images/Alita Battle Angel/landscape.jpg'
+	portrait: '/assets/images/Pushpa 2/portrait.png',
+	landscape: '/assets/images/Pushpa 2/landscape.jpg'
 },
 {
 	id: 3,
-	name: 'The Lion King',
+	name: 'KGF 3',
 	price: 579,
-	portrait: '/assets/images/The Lion King/portrait.jpg',
-	landscape: '/assets/images/The Lion King/landscape.webp'
+	portrait: '/assets/images/KGF 3/portrait.jpg',
+	landscape: '/assets/images/KGF 3/landscape.webp'
 },
 {
 	id: 4,
-	name: 'Toy Story 4',
+	name: 'Salaar',
 	price: 199,
-	portrait: '/assets/images/Toy Story 4/portrait.jpg',
-	landscape: '/assets/images/Toy Story 4/landscape.jpg'
+	portrait: '/assets/images/Salaar/portrait.jpg',
+	landscape: '/assets/images/Salaar/landscape.jpg'
 },
 {
 	id: 5,
-	name: 'How To Train Your Dragon',
+	name: 'Project K',
 	price: 239,	
-	portrait: '/assets/images/How To Train Your Dragon/portrait.jpg',
-	landscape: '/assets/images/How To Train Your Dragon/landscape.jpg'
+	portrait: '/assets/images/Project K/portrait.jpg',
+	landscape: '/assets/images/Project K/landscape.jpg'
 },
 {
 	id: 6,
-	name: 'Thor Ragnarok',
+	name: 'NTR-31',
 	price: 334,
-	portrait: '/assets/images/Thor Ragnarok/portrait.jpg',
-	landscape: '/assets/images/Thor Ragnarok/landscape.jpg'
+	portrait: '/assets/images/NTR-31/portrait.jpg',
+	landscape: '/assets/images/NTR-31/landscape.jpg'
 },
 {
 	id: 7,
-	name: 'Avengers Infinity War',
+	name: 'Game Changer',
 	price: 459,
-	portrait: '/assets/images/Avengers Infinity War/portrait.jpg',
-	landscape: '/assets/images/Avengers Infinity War/landscape.jpg'
+	portrait: '/assets/images/Game Changer/portrait.jpg',
+	landscape: '/assets/images/Game Changer/landscape.jpg'
 },
 {
 	id: 8,
-	name: 'Spiderman: Homecoming',
+	name: 'Orange',
 	price: 630,
-	portrait: '/assets/images/Spiderman: Homecoming/portrait.jpg',
-	landscape: '/assets/images/Spiderman: Homecoming/landscape.jpg'
+	portrait: '/assets/images/Orange/portrait.jpg',
+	landscape: '/assets/images/Orange/landscape.jpg'
 }
 ])
 
